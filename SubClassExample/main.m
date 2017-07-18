eb2e15cd369542f30e4696a42b748ca12f6a4cb4
test <test@tests-Mac.local>
2017-07-18 09:38:31 -0200

    Initial Commit

---
+++ SubClassExample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SubClassExample
+//
+//  Created by test on 7/18/17.
+//  Copyright © 2017 com.ITContractors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
